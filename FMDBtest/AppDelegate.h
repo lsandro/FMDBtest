2bbd43b1b7d1c781cb5f5d543299406638f531a9
lsandro <42097314@qq.com>
2016-09-18 19:54:38 +0800

    Initial Commit

---
+++ FMDBtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FMDBtest
+//
+//  Created by YI on 16/9/18.
+//  Copyright © 2016年 Sandro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FMDBtest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FMDBtest
+//
+//  Created by YI on 16/9/18.
+//  Copyright © 2016年 Sandro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
